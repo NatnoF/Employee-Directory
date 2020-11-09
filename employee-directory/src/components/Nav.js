db741a753a880fd19c375f59e663b6ec195c3084
Nicolas Fontan <nico.fontan1@gmail.com>
2020-11-09 16:33:57 -0500

    Homework startup again

---
+++ employee-directory/src/components/Nav.js
@@ -0,0 +1,14 @@
+import SearchBox from "./SearchBox";
+import "../stlyes/Nav.css";
+
+function Nav({ handleSearchChange }) {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <SearchBox handleSearchChange={handleSearchChange} />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
